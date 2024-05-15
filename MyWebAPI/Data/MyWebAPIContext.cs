7f05a5300da6b5670235ac9d8eb6a58c5a3b30d1
Mohammad Abu Snober <mohammad.abusnober@hotmail.com>
2024-05-15 18:13:20 +0300

    Add project files.

---
+++ MyWebAPI/Data/MyWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyWebAPI.Models;
+
+namespace MyWebAPI.Data
+{
+    public class MyWebAPIContext : DbContext
+    {
+        public MyWebAPIContext (DbContextOptions<MyWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyWebAPI.Models.Blog> Blog { get; set; } = default!;
+    }
+}
---
+++ MyWebAPI/Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebAPI.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string btitle { get; set; }
+        [Required]
+        public string author { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateOnly cdate { get; set; }
+        [MaxLength(100)]
+        public string comments { get; set; }
+    }
+}
